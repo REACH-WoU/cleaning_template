f3010b60e54d1b3d1d25abc9650e4280866de9c2
Nestor-Ch <132923140+Nestor-Ch@users.noreply.github.com>
2024-01-30 11:36:29 +0200

    added a check for consistency of var names

--- src/sections/tool_modification.R
+++ src/sections/tool_modification.R
@@ -1,3 +1,24 @@
+names_tool <- tool.survey %>% 
+  filter(grepl('(select_one)|(select_multiple)|(integer)|(decimal)',type)) %>% pull(name)
+
+names_list <- names(raw.main)
+
+if(length(sheet_names_new)>0){
+  for(frame in sheet_names_new){
+    txt <- paste0("names(",frame,")")
+    names_loop <- eval(parse(text=txt))
+    names_list <- c(names_list,names_loop)
+  }
+}
+
+names_missing <- setdiff(names_tool,names_list)
+
+if(length(names_missing)>0){
+  
+  stop((paste0("some of the names in your tool are not present in your dataframe. Please double check if they were renamed: ",
+                  paste0(names_missing,collapse = ',\n'))))
+}
+
 # # Find the question column in the XLSForm
 # question_column <- "reasons_for_displacement_opt"
 # new_option <- "cash_payments"
