2d052dced1a15bdcbf4f1f1e2226d7df46f16c59
Bohdan213 <bogdanpeleh213@gmail.com>
2024-05-23 14:58:21 +0300

    fixes + partial cleaning functionality

--- src/sections/section_4_create_other_requests_files.R
+++ src/sections/section_4_create_other_requests_files.R
@@ -32,16 +32,24 @@ if(length(sheet_names_new)>0){
 
 if(nrow(other.responses)>0){
   
-# translate your data
-other.responses.j <- utilityR::translate.responses(responses = other.responses,
-                                                   values_from = 'responses',
-                                                   directory = directory_dictionary$dir.requests,
-                                                   api.key = api_key
-)
-
-utilityR::save.other.requests(utilityR::create.translate.requests(other.responses.j),
-                              directory = directory_dictionary$dir.requests,
-                              make.short.name("other_requests_final"), use_template = F)
+  if (partial_clean) {
+    existed.requests  <- utilityR::load.requests(directory_dictionary$dir.requests,
+                                                 other_request_file_pattern,
+                                                 sheet = sheet_name_others, validate = T)
+    # remove from other.responses.j where uuid in or.edited$uuid
+    other.responses <- other.responses[!other.responses$uuid %in% existed.requests$uuid,]
+  }
+    
+  # translate your data
+  other.responses.j <- utilityR::translate.responses(responses = other.responses,
+                                                     values_from = 'responses',
+                                                     directory = directory_dictionary$dir.requests,
+                                                     api.key = api_key
+  )
+  
+  utilityR::save.other.requests(utilityR::create.translate.requests(other.responses.j),
+                                directory = directory_dictionary$dir.requests,
+                                make.short.name(other_request_file_name), use_template = F)
 }
 
 # ------------------------------------------------------------------------------
@@ -89,15 +97,22 @@ if(length(sheet_names_new)>0){
   }
 }
 if(nrow(trans.responses)>0){
-# translate all of the responses
-trans.responses.j <- utilityR::translate.responses(responses = trans.responses,
-                                                   values_from = 'responses',
-                                                   directory = directory_dictionary$dir.requests,
-                                                   api.key = api_key)
-
-# save the translation requests
-utilityR::save.trans.requests(utilityR::create.translate.requests(trans.responses.j),
-                              directory = directory_dictionary$dir.requests,
-                              make.short.name("text_requests_final"), use_template = F)
+  if (partial_clean) {
+    existed.requests  <- utilityR::load.requests(directory_dictionary$dir.requests,
+                                                 text_request_file_pattern,
+                                                 sheet = sheet_name_others, validate = T)
+    # remove from other.responses.j where uuid in or.edited$uuid
+    trans.responses <- trans.responses[!trans.responses$uuid %in% existed.requests$uuid,]
+  }
+  # translate all of the responses
+  trans.responses.j <- utilityR::translate.responses(responses = trans.responses,
+                                                     values_from = 'responses',
+                                                     directory = directory_dictionary$dir.requests,
+                                                     api.key = api_key)
+  
+  # save the translation requests
+  utilityR::save.trans.requests(utilityR::create.translate.requests(trans.responses.j),
+                                directory = directory_dictionary$dir.requests,
+                                make.short.name(text_request_file_name), use_template = F)
 
 }
