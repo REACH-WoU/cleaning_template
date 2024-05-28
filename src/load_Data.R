e244d407a8682b6644aa2ee51a83374b3a2eeffa
Nestor-Ch <132923140+Nestor-Ch@users.noreply.github.com>
2024-05-28 09:58:19 +0300

    changes to load data functionality (xlsx pattern recognition) and adding the partial other cleaning functionality

--- src/load_Data.R
+++ src/load_Data.R
@@ -3,17 +3,17 @@
 # and loads the data into kobo.raw.main, kobo.raw.loop1...
 # also included are the standard steps of renaming uuid, and adding the loop_index
 
-raw_data_filename <- list.files(directory_dictionary$data_path, full.names = T)
+raw_data_filename <- list.files(directory_dictionary$data_path, full.names = T, pattern = 'xlsx$')
 
 if(length(raw_data_filename) > 1) { stop("Found multiple files containing raw Kobo data! Please clean up the kobo_export folder.")
-}else if(length(raw_data_filename) == 0){ 
+}else if(length(raw_data_filename) == 0){
   warning("Raw Kobo data not found!")
   kobo.raw.main <- data.frame()
   kobo.raw.loop1 <- data.frame()
   dataset_creation_time <- NA
   dctime_short <- ""
 }else if(length(raw_data_filename) == 1){
-  
+
   ls <- excel_sheets(path = raw_data_filename)
   sheet_names <- if(length(ls)>1){
     c('kobo.raw.main',paste0('kobo.raw.loop',1:(length(ls)-1)))
@@ -25,23 +25,23 @@ if(length(raw_data_filename) > 1) { stop("Found multiple files containing raw Ko
       kobo.raw.main <- readxl::read_xlsx(raw_data_filename, col_types = "text", sheet = ls[i]) %>%
         rename(uuid = "_uuid",
                submission_time = "_submission_time",
-               index = "_index") %>% 
-        rename_all(~sub("_geolocation","geolocation", .x)) 
+               index = "_index") %>%
+        rename_all(~sub("_geolocation","geolocation", .x))
     }else{
-      txt <- paste0(sheet_names[i],'=readxl::read_xlsx(raw_data_filename, col_types = "text", sheet = "',ls[i],'")%>% 
+      txt <- paste0(sheet_names[i],'=readxl::read_xlsx(raw_data_filename, col_types = "text", sheet = "',ls[i],'")%>%
     rename(uuid = "_submission__uuid",
            loop_index = "_index",
            parent_index = "_parent_index",
            submission_id = "_submission__id",
-           submission_submission_time ="_submission__submission_time") %>% 
+           submission_submission_time ="_submission__submission_time") %>%
     mutate(loop_index = paste0("loop',i-1,'_", loop_index))'
       )
       eval(parse(text = txt))
     }
   }
-  
+
   dataset_creation_time <- as.Date(file.info(raw_data_filename)$ctime)
-  dctime_short <- str_extract(gsub('-', '', str_sub(dataset_creation_time, 3)), "\\d+") 
+  dctime_short <- str_extract(gsub('-', '', str_sub(dataset_creation_time, 3)), "\\d+")
 }
 
 
