2fdfc51611cbeeacec1d9c81f227945481975ac1
Bohdan213 <93724511+Bohdan213@users.noreply.github.com>
2024-10-02 15:40:45 +0300

    Update section_x_anomaly.R

--- src/sections/section_x_anomaly.R
+++ src/sections/section_x_anomaly.R
@@ -78,7 +78,214 @@ df_enum_tests <- list(
   'Enumerator duration problems' = issues_test %>% arrange(desc(perc_issues))
 )
 
-write.xlsx(paste0(directory_dictionary$dir.audits.check,'Enumerator_anomalies.xlsx'))
+write.xlsx(df_enum_tests, paste0(directory_dictionary$dir.audits.check, 'Enumerator_anomalies_time.xlsx'))
 
 
+################## chain problems ################## 
+# check.questions <- c("H_2_water_source", "H_5_water_assess", "H_6_water_treat",
+#                      "H_10_wash_issues", "H_12_hand_washing", "H_14_toilet", "H_17_safe_toilet")
+
+# check.questions <- c("fsl_fcs_cereal", "fsl_fcs_legumes", "fsl_fcs_dairy", "fsl_fcs_meat",
+#                    "fsl_fcs_veg", "fsl_fcs_fruit", "fsl_fcs_oil", "fsl_fcs_sugar", "I_11_struggle_enough_money")
+
+check.logic.questions <- c("B_15_wgss_seeing", "B_16_wgss_hearing",
+                     "B_17_wgss_walking", "B_18_wgss_cognit", "B_19_wgss_selfcare",
+                     "B_20_wgss_comm")
+
+geo_column <- "macroregion"
+
+calculate.markov.outliers <- function(data, questions, geo_column) {
+  
+  fit_markov_chain <- function(interviews, states) {
+    transition_matrix <- matrix(0, nrow = length(states), ncol = length(states))
+    rownames(transition_matrix) <- states
+    colnames(transition_matrix) <- states
+    
+    for (interview in interviews) {
+      for (i in 1:(length(interview) - 1)) {
+        from_state <- interview[i]
+        to_state <- interview[i + 1]
+        transition_matrix[from_state, to_state] <- transition_matrix[from_state, to_state] + 1
+      }
+    }
+    
+    row_sums <- rowSums(transition_matrix)
+    
+    for (i in 1:nrow(transition_matrix)) {
+      if (row_sums[i] == 0) {
+        transition_matrix[i, ] <- 1 / ncol(transition_matrix)
+      } else {
+        transition_matrix[i, ] <- transition_matrix[i, ] / row_sums[i]
+      }
+    }
+    
+    mc <- new("markovchain", states = states, transitionMatrix = transition_matrix)
+    
+    return(mc)
+  }
+  
+  calculate_likelihood <- function(interview, mc_model) {
+    log_prob <- 0
+    for (i in 1:(length(interview) - 1)) {
+      log_prob <- log_prob + log(mc_model[as.character(interview[i]), as.character(interview[i+1])])
+    }
+    return(log_prob)
+  }
+  
+  geovalues <- unlist(unique(data[geo_column]))
+  res_list <- c()
+  
+  suspected_data_combined <- data.frame()
+  
+  for(geovalue in geovalues) {
+    logic.check.data <- data %>%
+      dplyr::filter(!!sym(geo_column) %in% geovalue) %>%
+      dplyr::select(c("uuid", directory_dictionary$enum_colname, questions))
+    
+    logic.check.data <- logic.check.data %>% drop_na()
+    
+    interviews <- list()
+    for (i in 1:nrow(logic.check.data)) {
+      row <- logic.check.data[i, ]
+      interview <- row[questions] %>% as.list() %>% unlist()
+      for (idx in 1:length(interview)) {
+        interview[idx] <- paste0(questions[idx], ":", interview[idx])
+      }
+      interviews[[i]] <- interview
+    }
+    
+    states <- c(unique(unlist(interviews)))
+    
+    mc_model <- fit_markov_chain(interviews, states)
+    
+    likelihoods <- sapply(interviews, calculate_likelihood, mc_model = mc_model)
+    
+    outliers <- which(likelihoods <= quantile(likelihoods, 0.05))
+    
+    suspected_data <- logic.check.data[outliers, ]
+    
+    suspected_data$probs <- likelihoods[outliers]
+    
+    suspected_data %>% dplyr::select(c("uuid", directory_dictionary$enum_colname, questions, "probs"))
+    
+    enum_outlier_count <- suspected_data %>%
+      dplyr::group_by(enum) %>%
+      dplyr::summarize(outlier_count = n()) 
+    
+    
+    enum_interview_count <- logic.check.data %>%
+      dplyr::group_by(enum) %>%
+      dplyr::summarize(count = n()) %>%
+      dplyr::left_join(enum_outlier_count, by = "enum") %>%
+      dplyr::mutate(outlier_count = ifelse(is.na(outlier_count), 0, outlier_count),
+                    probs = outlier_count / count)
+    
+    enum_interview_count$probs <- enum_interview_count$outlier_count / enum_interview_count$count
+    
+    enum_interview_count <- enum_interview_count %>%
+      dplyr::filter(count > 15 & probs > 0.075)
+    
+    suspected_data <- suspected_data %>%
+      dplyr::filter(!!sym(directory_dictionary$enum_colname) %in% enum_interview_count$enum)
+    
+    suspected_data$geovalue <- geovalue
+    
+    suspected_data_combined <- rbind(suspected_data_combined, suspected_data)
+    
+    res_list <- append(res_list, setNames(list(enum_interview_count), geovalue))
+  }
+  
+  return(list(res_list = res_list, suspected_data = suspected_data_combined))
+}
+
+raw.main <- data
+
+if (length(check.logic.questions) != 0) {
+  
+  res <- calculate.markov.outliers(raw.main, check.questions, geo_column)
+  
+  prob_res <- res$res_list
+  suspected_data <- res$suspected_data
+  suspected_data$probs <- exp(suspected_data$probs)
+  suspected_data$probs <- paste0(round(suspected_data$probs * 100, 6), "%")
+  
+  res_df <- data.frame()
+  for (geovalue in names(prob_res)) {
+    prob_res[[geovalue]]$geovalue <- geovalue
+    res_df <- rbind(res_df, prob_res[[geovalue]])
+  }
+  
+  res_df$probs <- paste0(round(res_df$probs * 100, 2), "%")
+  
+  write.xlsx(res_df, 'Enumerator_anomalies_logic.xlsx')
+  write.xlsx(suspected_data, 'Enumerators_anomalies_logic_data.xlsx')
+} else {
+  cat("Columns for logic check wasn't defined, process skipped")
+}
+
+################## entropy check ################## 
+
+entropy.process <- function(data, exclude_columns = c("uuid"), enum_id.column = "enum_id") {
+  
+  calculate_entropy <- function(responses) {
+    responses <- na.omit(responses)
+    if (length(responses) == 0) {
+      return(0)
+    }
+    
+    split_responses <- strsplit(responses, " ")
+    all_answers <- unlist(split_responses)
+    
+    response_freq <- table(all_answers)
+    probabilities <- response_freq / sum(response_freq)
+    
+    entropy::entropy(probabilities, unit = "log2")
+  }
+  
+  enumerator_entropy <- data %>%
+    dplyr::group_by(!!sym(directory_dictionary$enum_colname)) %>%
+    dplyr::summarize(
+      across(
+        .cols = -all_of(exclude_columns),
+        .fns = ~calculate_entropy(.x),
+        .names = "{col}_entropy"
+      ),
+      num_interviews = n()
+    )
+  
+  return(enumerator_entropy)
+}
+
+
+if (length(check.logic.questions) != 0) {
+  entropy_res_list <- list()
+  geovalues <- unlist(unique(data[geo_column]))
+  
+  for (geovalue in geovalues) {
+    entropy_data <- raw.main %>%
+      dplyr::filter(!!sym(geo_column) %in% geovalue) %>%
+      dplyr::select(c(check.logic.questions, "uuid", directory_dictionary$enum_colname))
+    
+    print(colnames(entropy_data))
+    
+    entropy_sum <- entropy.process(entropy_data, exclude_columns = c("uuid"), enum_id.column = directory_dictionary$enum_colname) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(
+        entropy_sum = sum(
+          across(
+            .cols = -c(enum, num_interviews)
+          ),
+          na.rm = TRUE
+        )
+      ) %>%
+      dplyr::select("enum", "entropy_sum", "num_interviews")
+    
+    entropy_sum <- entropy_sum %>%
+      dplyr::filter(num_interviews > 15)
+    
+    entropy_res_list <- append(entropy_res_list, setNames(list(entropy_sum), geovalue))
+  }
+}
+
+################## cross check ################## 
 
